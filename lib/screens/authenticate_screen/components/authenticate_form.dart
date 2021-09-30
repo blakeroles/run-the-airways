5cdcdba49b5b16e794efc05d56271aea08e7701b
Blake Roles <blakeroles@gmail.com>
2021-10-01 07:33:50 +1000

    Working on authenticate screen, adding dependency

--- lib/screens/authenticate_screen/components/authenticate_form.dart
+++ lib/screens/authenticate_screen/components/authenticate_form.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+// This has been adapted from an example in https://github.com/dreampowder/strava_flutter/blob/master/example/lib/main.dart
+// using the strava_flutter package
 
 class AuthenticateForm extends StatefulWidget {
   @override
@@ -6,6 +10,9 @@ class AuthenticateForm extends StatefulWidget {
 }
 
 class _AuthenticateFormState extends State<AuthenticateForm> {
+  final TextEditingController _textEditingController = TextEditingController();
+  final DateFormat datFormatter = DateFormat("HH:mm:ss");
+
   @override
   Widget build(BuildContext context) {
     return Container();
