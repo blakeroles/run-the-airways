bbbaf62c93d42355578d90e29adf95c8b48106ac
Blake Roles <blakeroles@gmail.com>
2021-10-02 13:36:09 +1000

    Working on oauth

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFFBB0707);
 const kTextColor = Color(0xFF757575);
+
+final String scope = "activity:write,read";
--- lib/screens/authenticate_screen/components/authenticate_form.dart
+++ lib/screens/authenticate_screen/components/authenticate_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:strava_flutter/strava.dart';
+import 'package:run_the_airways/secret.dart';
+import 'package:run_the_airways/constants.dart';
 
 // This has been adapted from an example in https://github.com/dreampowder/strava_flutter/blob/master/example/lib/main.dart
 // using the strava_flutter package
@@ -11,11 +13,39 @@ class AuthenticateForm extends StatefulWidget {
 }
 
 class _AuthenticateFormState extends State<AuthenticateForm> {
-  final TextEditingController _textEditingController = TextEditingController();
-  final DateFormat datFormatter = DateFormat("HH:mm:ss");
+  void stravaAuthenticate() {
+    authenticate(secret);
+  }
+
+  void authenticate(String secret) async {
+    bool isAuthOk = false;
+
+    final strava = Strava(true, secret);
+    final prompt = "auto";
+
+    isAuthOk = await strava.oauth(clientId, scope, secret, prompt);
+
+    if (isAuthOk) {
+      print("Auth OK!");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(''),
+          Text('Authentication'),
+          Text('with Strava'),
+          ElevatedButton(
+            key: Key('AuthenticationButton'),
+            child: Text('Authenticate'),
+            onPressed: stravaAuthenticate,
+          )
+        ],
+      ),
+    );
   }
 }
